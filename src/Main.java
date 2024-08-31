fe5ae9cce21b99296ba1e7bf11d9417c424c2057
Matvey Odintsov <126068049+at0m-cat@users.noreply.github.com>
2024-08-31 16:04:05 +0700

    action sketch

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,44 @@ public class Main {
     private static final String EXIT_GAME_INFO = "Выйти из игры?";
     private static final String START_GAME = "start";
     private static final String EXIT_GAME = "exit";
-    private static final File file = new File("src/russian-nouns.txt");
-    private static final List<String> strings;
+    private static final File FILE = new File("src/russian-nouns.txt");
+    private static final List<String> STRINGS;
+    private static int errorCount = 0;
 
     static {
         try {
-            strings = Files.readAllLines(file.toPath());
+            STRINGS = Files.readAllLines(FILE.toPath());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public static void printedGallows(){
+        // прибавляем к счетчику ошибок 1,
+        // рисуем части виселицы (6)
+    }
+
+    public static void generationWord(){
+        // генерируем слово
+    }
+
+    public static void openingLetters(){
+        // если в слове есть буква - открываем, изначально слово замаскировано
+    }
+
+    public static void endedGame(){
+        // выход из игры
+    }
+
+    public static void startGame(){
+        // цикл игры
+    }
+
+    public static void action(){
+        // выбор действия - новая игра , выход из игры
+    }
+
     public static void main(String[] args) {
-        for (String string : strings){
-            System.out.println(string);
-        }
+
     }
 }
